e6b74cbf5e14234bc2e60b4c911743e6d3fa79aa
Monstermel <abreualvarez@hotmail.com>
2021-11-04 22:55:05 -0600

    Se removieron comentarios extra

--- src/main.cpp
+++ src/main.cpp
@@ -16,25 +16,6 @@ int main(int argc, char* argv[]) {
             std::cout << "Error: " << msg << std::endl;
             return EXIT_FAILURE;
         }
-
-        /*
-        // Solve
-        problem.solve();
-
-        if (problem.must_be_fixed()) {
-            std::cout << "El problema esta mal planteado" << std::endl;
-            return EXIT_FAILURE;
-        }
-
-        if (problem.has_solutions()) {
-            if (!problem.is_unlimited())
-                problem.print_solution();
-            else
-                std::cout << "Problema con infinitas soluciones." << std::endl;
-        } else {
-            std::cout << "El problema es infactible" << std::endl;
-        }
-        */
         return EXIT_SUCCESS;
     } else {
         std::cout << "Error: No se proporciono un problema" << std::endl;
